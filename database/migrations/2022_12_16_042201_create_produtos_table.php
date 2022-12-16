55d8b6db7d649116b814a861b910b36f8a122004
Matondo <matondojoaokitemoco@gmail.com>
2022-12-16 08:35:10 +0100

    Criando o Model e Migration de produtos

---
+++ database/migrations/2022_12_16_042201_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->longText('descricao');
+            $table->double('valor_compra');
+            $table->double('valor_venda');
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
